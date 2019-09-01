fd9f20f221bea8b7638a313bc17644d81f950bbf
邹宇鸿 <790381001@qq.com>
2019-09-01 16:09:52 +0800

    xianyunday1

---
+++ pages/tab.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+  <div class="tabs" style="margin:30px">
+      <span
+       v-for="(item , index) in ['娱乐','新闻','体育']" 
+       :key="index"
+       @click="list(index)"
+        :class="{ active:current === index}"
+       >{{item}}</span>      
+  </div>
+  <div v-show="current ===0">
+      <ul>
+          <li>代码战神邹柯</li>
+          <li>代码战神邹柯</li>
+          <li>代码战神邹柯</li>
+      </ul>
+  </div>
+  <div v-show="current===1">
+      <ul>
+          <li>代码战皇邹柯</li>
+          <li>代码战皇邹柯</li>
+          <li>代码战皇邹柯</li>
+      </ul>
+  </div>
+  <div v-show="current===2">
+      <ul>
+          <li>代码斗罗邹柯</li>
+          <li>代码斗罗邹柯</li>
+          <li>代码斗罗邹柯</li>
+      </ul>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+        data() {
+            return {
+                current:0
+            }
+        },
+        methods:{
+            list(index){
+                this.current = index
+            }
+        }
+}
+</script>
+
+<style lang='less' scoped>
+ .tabs{
+        margin-bottom:20px;
+
+        span{
+            cursor: pointer;
+        }
+    }
+  .active{
+      color: red
+  }
+</style>
